1043563d81d4994d17f146acf0f670c735e09a8e
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-04-27 11:45:29 -0400

    cleaned up formatting

--- exercise_function.py
+++ exercise_function.py
@@ -3,11 +3,7 @@
 def full_names(first_name, last_name):
     formatted_names = [f"{first_name[i]} {last_name[i]}" for i in range(len(first_name))]
 
-    
-    
     print(formatted_names)
 
 full_names(['John', 'Evan', 'Jordan', 'Max'], ['Smith', 'Smith', 'Williams', 'Bell'])       
 
-first_name = ['John', 'Evan', 'Jordan', 'Max']
-last_name = ['Smith', 'Smith', 'Williams', 'Bell']
